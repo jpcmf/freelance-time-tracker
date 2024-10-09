0f038b622b838bcb3794ec88f95be27c7ff033b8
João Paulo Fricks <jpfricks@gmail.com>
2024-10-08 23:14:09 -0300

    Create user models

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,7 +13,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        User::factory()
+            ->count(200)
+            ->create();
 
         User::factory()->create([
             'name' => 'Test User',
