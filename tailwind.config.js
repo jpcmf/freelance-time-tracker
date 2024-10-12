e7ff25176fbda3f4bc4e2faca71628948935d1a4
João Paulo Fricks <jpfricks@gmail.com>
2024-10-12 20:53:44 -0300

    Add tailwind configuration

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: ["./resources/**/*.blade.php"],
+    theme: {
+        extend: {
+            font: {
+                sans: ["Segoe UI", "sans-serif"],
+            },
+        },
+    },
+    plugins: [],
+};
