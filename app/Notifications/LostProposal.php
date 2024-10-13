b5be865b23438c3928cb3a8501a216c877e962c4
João Paulo Fricks <jpfricks@gmail.com>
2024-10-13 20:11:24 -0300

    Implement queue

--- app/Notifications/LostProposal.php
+++ app/Notifications/LostProposal.php
@@ -8,7 +8,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
-class LostProposal extends Notification
+class LostProposal extends Notification implements ShouldQueue
 {
     use Queueable;
 
