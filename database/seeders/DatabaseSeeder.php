fe01fe1b2d798324ad6010d5638be26874b1baa6
João Paulo Fricks <jpfricks@gmail.com>
2024-10-13 19:35:51 -0300

    Update proposals positions

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,11 +2,13 @@
 
 namespace Database\Seeders;
 
+use App\Actions\ArrangePositions;
 use App\Models\Project;
 use App\Models\Proposal;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -27,6 +29,8 @@ class DatabaseSeeder extends Seeder
                     Proposal::factory()
                         ->count(random_int(4, 45))
                         ->create(['project_id' => $project->id,]);
+
+                    ArrangePositions::run($project->id);
                 }
             );
     }
